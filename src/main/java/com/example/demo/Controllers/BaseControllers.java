975ad1c31ebfb17a84b947c60b07380258e2625a
StefaniPetkova <stefani.r.petkova@gmail.com>
2021-03-16 20:41:59 +0200

    .

--- src/main/java/com/example/demo/Controllers/BaseControllers.java
+++ src/main/java/com/example/demo/Controllers/BaseControllers.java
@@ -1,19 +1,25 @@
 package com.example.demo.Controllers;
 
-import com.example.demo.Services.CalculationService;
+import com.example.demo.Services.ICalculationService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class BaseControllers {
 
     @Autowired
-    private CalculationService calculationService;
+    private ICalculationService calculationService;
 
     @GetMapping("/calculate")
-    public List<String> calculate(@RequestParam int disks){
+    @ApiOperation(value = "Solves the Hanoi Towers problem")
+
+
+    public List<String> calculate(@ApiParam(value = "Required integer value greater than 0") @RequestParam int disks){
         List<String> steps= calculationService.calculate(disks, 'A', 'C', 'B');
         return steps;
     }
--- src/main/java/com/example/demo/Services/CalculationService.java
+++ src/main/java/com/example/demo/Services/CalculationService.java
@@ -1,7 +1,27 @@
 package com.example.demo.Services;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public interface CalculationService {
-    List<String> calculate(int disks, char from_rod,char to_rod, char aux_rod);
+@Service
+public class CalculationService implements ICalculationService {
+
+
+    @Override
+    public List<String> calculate(int disks, char fromRod, char toRod, char auxRod) {
+        List<String> steps = new ArrayList<>();
+        if (disks == 1)
+        {
+            steps.add("Move disk 1 from rod " + fromRod + " to rod " + toRod);
+            return steps;
+        }
+
+        steps.addAll(calculate( disks - 1, fromRod, auxRod, toRod));
+        steps.add("Move disk " + disks + " from rod " + fromRod + " to rod " + toRod );
+        steps.addAll(calculate( disks - 1, auxRod, toRod, fromRod));
+
+        return steps;
+    }
 }
